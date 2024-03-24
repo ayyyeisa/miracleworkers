08a716ba2cfcf5c6e8737fe6c87aba101b8ac655
crynoso <cscheidecker@mail.bradley.edu>
2024-03-24 11:15:54 -0500

    Update sellerLandingPage.js

--- client/src/pages/sellerLandingPage.js
+++ client/src/pages/sellerLandingPage.js
@@ -1,12 +1,30 @@
 import React from "react";
- 
+
+
+ //Format for seller landing page
 const SellerLandingPage = () => {
     return (
+//navbar goes here
         <div>
             <h1>
-                This is the seller landing page.
+                3D Printing Architectural Trends in 2026 
+            </h1>
+                <img src="https://i.imgur.com/lmKI23l.png"alt="image"></img>
+           
+            <h1>
+                Just Added
+            </h1>
+            <h1>
+                Good Fit for Your Company
+            </h1>
+            <h1>
+                Smart Home Collection
+            </h1>
+            <h1>
+                Tiny Home
             </h1>
         </div>
+
     );
 };
  
