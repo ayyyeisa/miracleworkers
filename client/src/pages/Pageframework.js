242875f4436043bd8c9d6476ab5a9be8d564728a
Isa <80214167+ayyyeisa@users.noreply.github.com>
2024-03-24 22:21:54 -0500

    buyer nav bar added

--- client/src/pages/Pageframework.js
+++ client/src/pages/Pageframework.js
@@ -5,13 +5,14 @@
 
 import React from "react";
 
-import { BuyerNavBar, SellerNavBar} from "../components/Navbar";
+import { BuyerNavBar, SellerNavBar, SmallerSellerNavBar, SmallerBuyerNavBar} from "../components/Navbar";
 
 // This page component includes the navbar components.
 // Takes in the main page component as a paramater.
 export const BuyerPageFramework = ({ component }) => {
     return (
         <div className="app-container">
+            <SmallerBuyerNavBar/>
             <BuyerNavBar />
             <div className="content-container">
                 <div className="main-content">
@@ -25,6 +26,7 @@ export const BuyerPageFramework = ({ component }) => {
 export const SellerPageFramework = ({ component }) => {
     return (
         <div className="app-container">
+            <SmallerSellerNavBar />
             <SellerNavBar />
             <div className="content-container">
                 <div className="main-content">
