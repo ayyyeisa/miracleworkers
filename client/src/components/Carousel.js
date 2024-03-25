f19db2e4fb86bd9e6079eebeb40d48ac7f7168c9
Isa <80214167+ayyyeisa@users.noreply.github.com>
2024-03-25 07:32:07 -0500

    carousel started. has errors

---
+++ client/src/components/Carousel.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../styles/carousel.css";
+
+function Carousel() {
+
+  return(
+    <div className="carousel">
+        <div className="carouselSlider">
+            <img src="" alt="" />
+            <div className="carouselText"/>
+        </div>
+    </div>
+  );
+}
+
+export default Carousel;
