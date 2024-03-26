58e8844bdf4fdcecd369656ad29d0e750aa582bd
Isa <80214167+ayyyeisa@users.noreply.github.com>
2024-03-26 02:32:33 -0500

    product linksnot successfuly added

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "../styles/Product.css";
+import {Link} from 'react-router-dom'
 
-const Product = ({name, image, link}) => {
+const Product = ({name, image}) => {
 //make this whole thing a button/link?
     return(
         <div className="product">
-        <img onclick={link} src={image} alt="" />
+        <Link to="/dummyPages/sampleProduct">
+            <img src="" alt="" />
+        </Link>
         <div className="product_info">
                 <p>{name}</p>
             </div> 
