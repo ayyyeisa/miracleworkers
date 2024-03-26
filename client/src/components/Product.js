71df21d3de269e27d4662d7924de51ce548ced3f
Isa <80214167+ayyyeisa@users.noreply.github.com>
2024-03-26 02:22:12 -0500

    fixedformatting

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "../styles/Product.css";
+import {Link} from 'react-router-dom';
 
 const Product = ({name, image, link}) => {
 //make this whole thing a button/link?
     return(
         <div className="product">
-        <img onclick={link} src={image} alt="" />
+        
+        <Link to={link}>
+            <img src={image} alt="" />
+        </Link>
         <div className="product_info">
                 <p>{name}</p>
             </div> 
